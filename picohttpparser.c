d073a907e613ad18f7ea7d5e2897e18867b2f425
MITSUNARI Shigeo <herumi@nifty.com>
2014-12-25 16:35:53 +0900

    try to use avx2

--- picohttpparser.c
+++ picohttpparser.c
@@ -156,6 +156,34 @@ static const char* findchar_fast(const char* buf, const char* buf_end, const cha
 
 static const char* findchar_ctl(const char* buf, const char* buf_end, int* found)
 {
+#if 0
+  // find c in [0, 0x9) | (0x9, 0x20) | 0x7f
+//  const __m256i c00 = _mm256_setzero_si256();
+  const __m256i c09 = _mm256_set1_epi8(0x09);
+  const __m256i c20 = _mm256_set1_epi8(0x20);
+  const __m256i c7f = _mm256_set1_epi8(0x7f);
+  while (buf - buf_end >= 64) {
+    __m256i x0 = _mm256_loadu_si256((const __m256i*)buf);
+    __m256i x1 = _mm256_loadu_si256((const __m256i*)(buf + 32));
+    __m256i y0 = _mm256_cmpgt_epi8(c20, x0); // x0 < 0x20
+    __m256i y1 = _mm256_cmpgt_epi8(c20, x1); // x1 < 0x20
+    __m256i z0 = _mm256_cmpeq_epi8(x0, c09); // x0 == 0x09
+    __m256i z1 = _mm256_cmpeq_epi8(x1, c09); // x1 == 0x09
+    z0 = _mm256_andnot_si256(z0, y0); // (x0 != 0x09) & (x0 < 0x20)
+    z1 = _mm256_andnot_si256(z1, y1); // (x1 != 0x09) & (x1 < 0x20)
+	z0 = _mm256_or_si256(z0, _mm256_cmpeq_epi8(x0, c7f));
+	z1 = _mm256_or_si256(z1, _mm256_cmpeq_epi8(x1, c7f));
+    uint64_t v0 = _mm256_movemask_epi8(z0);
+    uint64_t v1 = _mm256_movemask_epi8(z1);
+    v0 |= (v1 << 32);
+    if (v0) {
+      int v = __builtin_clzl(v0 ^ 63);
+      *found = 1;
+      return buf + v;
+    }
+    buf += 64;
+  }
+#endif
   static const char ranges[] =
     "\0\010"
     /* allow HT */
@@ -165,7 +193,6 @@ static const char* findchar_ctl(const char* buf, const char* buf_end, int* found
     /* allow chars w. MSB set */
     ;
   return findchar_fast(buf, buf_end, ranges, sizeof(ranges) - 1, found);
-#endif
 }
 
 static const char* findchar_space(const char* buf, const char* buf_end, int* found)
@@ -190,24 +217,40 @@ static const char* findchar_space(const char* buf, const char* buf_end, int* fou
     uint64_t v0 = _mm256_movemask_epi8(z0);
     uint64_t v1 = _mm256_movemask_epi8(z1);
     v0 |= (v1 << 32);
-    int v = __builtin_clzl(v0 ^ 63);
-    if (v >= 0) {
+    if (v0) {
+      int v = __builtin_clzl(v0 ^ 63);
       *found = 1;
       return buf + v;
     }
     buf += 64;
   }
-  *found = 0;
-  return buf;
-#else
+#endif
   static const char ranges[] __attribute__((aligned(16))) = "\000\040\177\177";
   return findchar_fast(buf, buf_end, ranges, sizeof(ranges) - 1, found);
-#endif
 }
 
 // short
 static const char* findchar_colon(const char* buf, const char* buf_end, int* found)
 {
+#if 0 // #ifdef __AVX2__
+//  const __m256i c00 = _mm256_setzero_si256();
+  const __m256i c20 = _mm256_set1_epi8(' ');
+  const __m256i c3a = _mm256_set1_epi8(':');
+  // c == ':' || (0 <= c & c < ' ')
+  while (buf - buf_end >= 32) {
+    __m256i x0 = _mm256_loadu_si256((const __m256i*)buf);
+    __m256i y0 = _mm256_cmpeq_epi8(x0, c3a);
+    __m256i z0 = _mm256_cmpgt_epi8(c20, x0);
+    z0 = _mm256_or_si256(y0, z0);
+    uint64_t v0 = _mm256_movemask_epi8(z0);
+    if (v0) {
+      int v = __builtin_clzl(v0 ^ 31);
+      *found = 1;
+      return buf + v;
+    }
+    buf += 32;
+  }
+#endif
   static const char ranges[] __attribute__((aligned(16))) = "::\x00\037";
   return findchar_fast(buf, buf_end, ranges, sizeof(ranges) - 1, found);
 }
